f44cde8f47b53efb94064ceabdab0bf2518a38d0
takuyakawata <gavic.f522@gmail.com>
2023-12-08 15:06:40 +0900

    Model_1

---
+++ app/Models/LessonUserReservation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LessonUserReservation extends Model
+{
+    use HasFactory;
+}
