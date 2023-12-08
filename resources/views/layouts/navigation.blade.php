f3aab015734b4bc2723d0afd917d9f4cefcfc1e2
takuyakawata <gavic.f522@gmail.com>
2023-12-08 20:58:31 +0900

    views_1

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,41 +1,80 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between h-20">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
-
+                 <div class="shrink-0 items-center hidden space-x-8 sm:-my-px  sm:flex">
+            　　　　<img class="rounded-t-xl h-10 w-auto" src="{{ asset('images/susan_spot_teacher.png') }}" alt="Image Description">
+        　　　　</div>
+                
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('top')" :active="request()->routeIs('dashboard')">
+                        {{ __('TOP') }}
                     </x-nav-link>
                 </div>
+                @if(Auth::check()) 
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('watchlater.index')" :active="request()->routeIs('dashboard')">
+                        {{ __('あとでみる') }}
+                    </x-nav-link>
+                </div>
+                
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('reserve.userreserve')" :active="request()->routeIs('dashboard')">
+                        {{ __('ユーザー履歴') }}
+                    </x-nav-link>
+                </div>
+                
+                 <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('reserve.schoolreserve')" :active="request()->routeIs('dashboard')">
+                        {{ __('学校履歴') }}
+                    </x-nav-link>
+                </div>
+                @endif
+                
+                <!-- 🔽 検索画面へのリンクを追加 -->
+               <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+               <x-nav-link :href="route('search.index')" :active="request()->routeIs('search.input')">
+                        {{ __('授業検索') }}
+               </x-nav-link>
+              </div>
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                 
+                        <div class="px-4">
+                         @if(Auth::check()) 
+                            <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                         @else
+                            <div class=" sm:top-0 sm:right-0 p-6 text-right z-10">
+                                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900  focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">ログイン</a>
+                                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">登録</a>
+                            </div>
+                         @endif
+                        </div>
 
-                            <div class="ms-1">
+
+                            <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </div>
+                            
                         </button>
                     </x-slot>
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('ユーザー情報') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -45,7 +84,7 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('ログアウト') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -53,7 +92,7 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
+            <div class="-mr-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -67,21 +106,53 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('top')" :active="request()->routeIs('dashboard')">
+                {{ __('TOP') }}
             </x-responsive-nav-link>
         </div>
+            @if(Auth::check()) 
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('watchlater.index')" :active="request()->routeIs('dashboard')">
+                {{ __('あとでみる') }}
+            </x-responsive-nav-link>
+        </div>
+        
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('reserve.userreserve')"  :active="request()->routeIs('dashboard')">
+                {{ __('ユーザー履歴') }}
+            </x-responsive-nav-link>
+        </div>
+        
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('reserve.schoolreserve')" :active="request()->routeIs('dashboard')">
+                {{ __('学校履歴') }}
+            </x-responsive-nav-link>
+        </div>
+          @endif
+        
+        <div class="pt-2 pb-3 space-y-1">
+        <x-responsive-nav-link :href="route('search.index')" :active="request()->routeIs('search.input')">
+               {{ __('授業検索') }}
+        </x-responsive-nav-link>
+       </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
+                <div class="px-4">
+                    @if(Auth::check()) 
+                        <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                     @else
+                        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        </div>
+                     @endif
+                </div>
+   
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                    {{ __('プロフィール') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
@@ -91,7 +162,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        {{ __('ログアウト') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
