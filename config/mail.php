508b99fa4b54192cd49b6571191eb4ec88cca8f1
takuyakawata <gavic.f522@gmail.com>
2023-12-09 21:04:59 +0900

    mail_1

--- config/mail.php
+++ config/mail.php
@@ -100,8 +100,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'spot_teacher@tokidokineko.sakura.ne.jp'),
+        'name' => env('MAIL_FROM_NAME', 'spotteacher'),
     ],
 
     /*
