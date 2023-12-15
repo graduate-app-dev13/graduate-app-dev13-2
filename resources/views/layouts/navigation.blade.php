7594f32ab8341c3bf957b05eb51e199070b1cee4
takuyakawata <gavic.f522@gmail.com>
2023-12-15 17:15:35 +0900

    nav_4

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -47,19 +47,17 @@
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
+                    @if(Auth::check())
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-
                         <div class="px-4">
-                        @if(Auth::check())
                             <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                        </button>
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </div>
-
-                        </button>
                     </x-slot>
 
                     <x-slot name="content">
@@ -72,18 +70,36 @@
                             @csrf
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                                        this.closest('form').submit();">
                                 {{ __('ログアウト') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
-                        @else
-                            <div class=" sm:top-0 sm:right-0 p-6 text-right z-10">
-                                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900  focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">ログイン</a>
-                                {{-- <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">登録</a> --}}
+
+                    @else
+                    <x-slot name="trigger">
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900  focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                <div class="px-4">
+                                    <div class="font-medium text-base text-gray-800">{{ __('ログイン') }}</div>
+                                </div>
+                            </button>
+                        </a>
+                            <div class="ml-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
                             </div>
-                            @endif
-                        </div>
+                    </x-slot>
+                    </div>
+                    
+                    <x-slot name="content" >
+                        <x-dropdown-link>
+                            {{ __('なし') }}
+                        </x-dropdown-link>
+                    </x-slot>
+                    @endif
+                    </div>
                 </x-dropdown>
             </div>
 
@@ -98,6 +114,7 @@
             </div>
         </div>
     </div>
+
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
